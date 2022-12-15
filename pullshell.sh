5e6b023cae46018f07f72b3be6709df00ba2d1f3
Kings Koko <ebruskokos@gmail.com>
2022-12-15 16:27:39 -0600

    finish number gene

--- pullshell.sh
+++ pullshell.sh
@@ -14,4 +14,11 @@ sleep 2
 echo $[ $RANDOM % 40 + 10]
 sleep 2
 echo $[ $RANDOM % 45 + 5]
+sleep 2
+echo $[ $RANDOM % 30 + 20]
+sleep 2
+echo $[ $RANDOM % 20 + 20]
+echo Powerball Number 
+sleep 2
+echo $[ $RANDOM % 10 + 30]
 
