c155b5830353a49d7039348b70a9200e9d18a314
Kings Koko <ebruskokos@gmail.com>
2022-12-16 15:47:41 -0600

    file

--- pullshell.sh
+++ pullshell.sh
@@ -1,32 +1,32 @@
-#!/bin/bash
+!/bin/bash
 
 
-# echo "Lottery number Generator" 
+echo "Lottery number Generator" 
 
 
 
-# # sleep 2
+# sleep 2
 
-# # echo $RANDOM
-
-# # sleep 1
-
-# echo $(($RANDOM % 48 + 1))
-# sleep 0.5s
-# echo $[ $RANDOM % 40 + 9]
-# sleep 0.5s
-# echo $[ $RANDOM % 45 + 4]
-# sleep 0.5s
-# echo $[ $RANDOM % 30 + 19]
-# sleep 0.5s
-# echo $[ $RANDOM % 20 + 29]
-# echo Powerball Number 
-# sleep 1
-# echo -e "\e[0;32m$[ $RANDOM % 10 + 30]\e[0m" #add color to code!!!! to green 32m
-
+# echo $RANDOM
 
 # sleep 1
-# echo Goodluck
+
+echo $(($RANDOM % 48 + 1))
+sleep 0.5s
+echo $[ $RANDOM % 40 + 9]
+sleep 0.5s
+echo $[ $RANDOM % 45 + 4]
+sleep 0.5s
+echo $[ $RANDOM % 30 + 19]
+sleep 0.5s
+echo $[ $RANDOM % 20 + 29]
+echo Powerball Number 
+sleep 1
+echo -e "\e[0;32m$[ $RANDOM % 10 + 30]\e[0m" #add color to code!!!! to green 32m
+
+
+sleep 1
+echo Goodluck
 for i in seq 1 10
 min =1
 max=49
