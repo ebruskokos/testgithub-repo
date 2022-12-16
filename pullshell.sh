65e9f557736d4f8cbbbe9878ea6bfb919c0d149c
Kings Koko <ebruskokos@gmail.com>
2022-12-15 18:49:56 -0600

    main beta 1.0.0

--- pullshell.sh
+++ pullshell.sh
@@ -3,6 +3,8 @@
 
 echo "Lottery number Generator" 
 
+
+
 # sleep 2
 
 # echo $RANDOM
@@ -10,16 +12,16 @@ echo "Lottery number Generator"
 # sleep 1
 
 echo $(($RANDOM % 48 + 1))
-sleep 2
+sleep 0.5s
 echo $[ $RANDOM % 40 + 10]
-sleep 2
+sleep 0.5s
 echo $[ $RANDOM % 45 + 5]
-sleep 2
+sleep 0.5s
 echo $[ $RANDOM % 30 + 20]
-sleep 2
+sleep 0.5s
 echo $[ $RANDOM % 20 + 20]
 echo Powerball Number 
-sleep 2
+sleep 1
 echo -e "\e[0;31m$[ $RANDOM % 10 + 30]\e[0m" #add color to code
 
 sleep 2
